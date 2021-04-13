16d3cedc89fddbfcc26b13da71e664473e2b46f7
RNaomiGlans <rnglans@gmail.com>
2021-04-13 16:53:33 +0200

    Enable LSI and Brown clustering

--- src/DataProcessor.py
+++ src/DataProcessor.py
@@ -231,17 +231,17 @@ class DataProcessor(object):
         del tfidfs_features
 
         # LSI features
-        # if self.n_lsi > 0:
-        #     print("Training LSI!")
-        #     svd_model = TruncatedSVD(n_components=self.n_lsi, \
-        #                              algorithm='arpack', \
-        #                              n_iter=self.n_iter, random_state=self.random_state)
-        #     svd_matrix = svd_model.fit_transform(tfidfs)
-        #     features = np.append(features, svd_matrix, 1)
-        #     print(len(features[0]))
-        #     del tfidfs
-        #     del svd_matrix
-        #     print("Got LSI!")
+        if self.n_lsi > 0:
+            print("Training LSI!")
+            svd_model = TruncatedSVD(n_components=self.n_lsi, \
+                                     algorithm='arpack', \
+                                     n_iter=self.n_iter, random_state=self.random_state)
+            svd_matrix = svd_model.fit_transform(tfidfs)
+            features = np.append(features, svd_matrix, 1)
+            print(len(features[0]))
+            del tfidfs
+            del svd_matrix
+            print("Got LSI!")
 
         if not os.path.exists(self.ROOT_DIR + 'data/saved'):
             os.makedirs(self.ROOT_DIR + 'data/saved')
@@ -386,8 +386,8 @@ class DataProcessor(object):
         tweet_vector.append(self.get_tagged_user_rate(tweet_str, n_token))
         tweet_vector.append(self.get_uppercase_rate(tweet_str))
         tweet_vector.extend(self.get_sentiment_word_rate(tweet_str))
-        # for n in self.n_list:
-        #     tweet_vector.extend(self.get_brown_cluster_vector(tweet_str, n))
+        for n in self.n_list:
+            tweet_vector.extend(self.get_brown_cluster_vector(tweet_str, n))
         tweet_vector.extend(trying)
         return tweet_vector
 
